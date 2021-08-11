1cc22a2870007b28db15b9f65f7fd245bcf12406
GuilhermeTeixeiraViveiros <viveirosgt@gmail.com>
2021-08-11 15:45:47 -0300

    Inicia implementação do métodos de JSON

--- src/main/app.java
+++ src/main/app.java
@@ -2,16 +2,34 @@ package main;
 import entities.Car;
 import entities.Person;
 import entities.Tag;
-
 import java.io.*;
 import java.io.FileWriter;
 
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
+
+import java.util.ArrayList;
+
 public class app {
     public static void main(String[] args) throws IOException {
-        FileWriter file = new FileWriter("employees.json");
-        Car car = new Car("HCC9999", new Tag("TAG123456"), new Person("Guilherme", "123456"));
+        ArrayList<Car> cars = new ArrayList<Car>();
+        Car newcar = new Car("HCC9999", new Tag("TAG123456"), new Person("Guilherme", "123456"));
+        cars.add(newcar);
 
-        file.write("Oláaa");
+        JSONObject jsonOBJ = new JSONObject();
+
+        jsonOBJ.put("AssociatedTag", newcar.getAssociatedTag().getId());
+        jsonOBJ.put("Plate", newcar.getPlate());
+        jsonOBJ.put("Owner", newcar.getOwner().getId());
+
+        JSONArray JSONARRAY = new JSONArray();
+
+        JSONARRAY.add(jsonOBJ);
+        JSONARRAY.add(jsonOBJ);
+        JSONARRAY.add(jsonOBJ);
+
+        FileWriter file = new FileWriter("cars.json");
+        file.write(JSONARRAY.toJSONString());
         file.flush();
     }
 }
