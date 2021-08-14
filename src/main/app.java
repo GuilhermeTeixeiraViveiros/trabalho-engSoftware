4d3c5d07c6807e1a4de44a2cb63032eaa35724a3
GuilhermeTeixeiraViveiros <viveirosgt@gmail.com>
2021-08-14 16:56:21 -0300

    Refatora main

--- src/main/app.java
+++ src/main/app.java
@@ -13,35 +13,19 @@ public class app {
     public static void main(String[] args) throws IOException {
         Scanner menu = new Scanner (System.in);
 
-        System.out.print("##-- Toll --##\n\n");
-        System.out.print("|----------------------------|\n");
-        System.out.print("| Option 1 - Charge by PLATE |\n");
-        System.out.print("| Option 2 - Charge by TAG   |\n");
-        System.out.print("| Option 3 - Charge by IMAGE |\n");
-        System.out.print("| Option 4 - Exit            |\n");
-        System.out.print("|----------------------------|\n");
-        System.out.print("Insert an Option : ");
+        printMenu();
 
         switch (menu.nextInt()) {
             case 1:
-                System.out.print("\nCharge by plate");
-                System.out.print("\nInsert plate");
-                String plate = menu.next();
-                ChargeBusinessRules.chargeByPlate(plate);
+                chargeByPlate(menu);
                 break;
 
             case 2:
-                System.out.print("\nCharge by tag");
-                System.out.print("\nInsert tag");
-                String tag = menu.next();
-                ChargeBusinessRules.chargeByTAG(tag);
+                chargeByTag(menu);
                 break;
 
             case 3:
-                System.out.print("\nCharge by image\n");
-                System.out.print("\nInsert image path");
-                String path = menu.next();
-                ChargeBusinessRules.chargeByImage(path);
+                chargeByImage(menu);
                 break;
 
             default:
@@ -52,5 +36,48 @@ public class app {
                 System.out.print("\nBye!");
                 menu.close();
         }
+    }
+
+    private static void printMenu() {
+        System.out.print("##-- Toll --##\n\n");
+        System.out.print("|----------------------------|\n");
+        System.out.print("| Option 1 - Charge by PLATE |\n");
+        System.out.print("| Option 2 - Charge by TAG   |\n");
+        System.out.print("| Option 3 - Charge by IMAGE |\n");
+        System.out.print("| Option 4 - Exit            |\n");
+        System.out.print("|----------------------------|\n");
+        System.out.print("Insert an Option : ");
+    }
+
+    private static void chargeByImage(Scanner menu) {
+        System.out.print("\nCharge by image\n");
+        System.out.print("\nInsert image path");
+        String path = menu.next();
+        ChargeBusinessRules.chargeByImage(path);
+        return;
+    }
+
+    private static void chargeByTag(Scanner menu) {
+        System.out.print("\nCharge by tag");
+        System.out.print("\nInsert tag");
+        String tag = menu.next();
+        if(ChargeBusinessRules.chargeByTAG(tag) == -1) {
+            System.out.print("\nUnable to complete charge");
+            return;
+        }
+        System.out.print("\nVehicle charged successfully");
+        return;
+    }
+
+    private static void chargeByPlate(Scanner menu) {
+        System.out.print("\nCharge by plate");
+        System.out.print("\nInsert plate");
+        String plate = menu.next();
+        if(ChargeBusinessRules.chargeByPlate(plate) == -1){
+            System.out.print("\nUnable to complete charge");
+            return;
+        }
+        System.out.print("\nVehicle charged successfully");
+        return;
     }
 }
